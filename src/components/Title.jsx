ab363040a0911b25c5ae6f507a0308f72e1a5e6b
Nahdian <nahdian7@gmail.com>
2023-08-11 21:09:42 +0700

    deplooy vercel

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,3 +1,5 @@
+import quranLogo from '../assets/logo.png';
+
 function Title() {
     return (
         <section id="header" >
@@ -18,7 +20,7 @@ function Title() {
                     </a>
                 </div>
                 <div className="hidden sm:flex sm:justify-center">
-                    <img src="../assets/logo.png" className="w-52 lg:w-80 lg:h-80 relative" />
+                    <img src={quranLogo} className="w-52 lg:w-80 lg:h-80 relative" />
                 </div>
             </div>
         </section>
